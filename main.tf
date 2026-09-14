ed1a1b7a3525cd4e1de47d31ca5b1da3e0a56ef1
Francisco Franco <ffranco@Franciscos-MacBook-Pro.local>
2026-09-13 17:35:55 -0700

    adding main.tf to create ec2 instance

---
+++ main.tf
@@ -0,0 +1,42 @@
+# main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"  # change to your preferred region
+}
+
+# Grab the latest Amazon Linux 2023 AMI so you don't have to hardcode an ID
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"   # free-tier eligible
+
+  tags = {
+    Name = "terraform-example-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
